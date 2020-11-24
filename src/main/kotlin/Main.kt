3805d8a126ae85b1bdce99758759a778d50dc51c
noloerino <jhshi07@gmail.com>
2020-11-23 21:42:37 -0800

    Expose CLI args

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,18 @@
-fun main() {
-    val server =
-//            Server(XmlDocumentGenerator(), XMLDocumentUtils::documentToString)
-             Server<String>(JavaScriptCodeGenerator(), "DummyTest", "testWithGenerator")
-//            Server<String>(NumberGenerator(), "NumberTest", "testWithGenerator")
+import com.pholser.junit.quickcheck.generator.Generator
+
+fun main(args: Array<String>) {
+    require(args.size == 3) {
+        "arguments: <generator class name> <test class name> <test method name>"
+    }
+    val genClass = Class.forName(args[0])
+    val genInstance = genClass.newInstance() as Generator<*>
+    val server = Server(
+            genInstance,
+            args[1],
+            args[2]
+    )
+//             Server(XmlDocumentGenerator(), XMLDocumentUtils::documentToString)
+//             Server<String>(JavaScriptCodeGenerator(), "DummyTest", "testWithGenerator")
+//             Server<String>(NumberGenerator(), "NumberTest", "testWithGenerator")
     server.start()
 }
